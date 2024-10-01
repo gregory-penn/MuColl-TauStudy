a7e319ca72c9d38823a1c2911ef26c97cfb37189
Ethan Martinez <87774190+ethanmar@users.noreply.github.com>
2024-10-01 02:27:51 -0400

    Update and rename lcio_particle_gun.py to lcio_tau_gun.py

---
+++ lcio_tau_gun.py
@@ -0,0 +1,86 @@
+# Create lcio files with single tau events
+# E. Martinez, 10/01/2024
+
+# initialize environment:
+# export PYTHONPATH=${LCIO}/src/python:${ROOTSYS}/lib
+
+import math
+import random
+from array import array
+from g4units import deg
+
+# LCIO dependencies
+from pyLCIO import UTIL, EVENT, IMPL, IO, IOIMPL
+
+# Number of events per momentum bin
+nevt = 200
+
+# Output file
+outfile = "tau_gen.slcio"
+
+# Open output file
+wrt = IOIMPL.LCFactory.getInstance().createLCWriter( )
+wrt.open(outfile, EVENT.LCIO.WRITE_NEW) 
+
+#========== particle properties ===================
+random.seed()
+
+#generate uniform distribution in pT
+pT = []
+for i in range(500):
+    pT.append(random.random()*300.+20) # 20-320 GeV/c
+
+genstat  = 1
+pdg = 15 # tau
+mass =  1.77686 # GeV/c^2
+charge = -1.
+decayLen = 1.e22 
+#=================================================
+
+
+
+for pt in pT:
+    for j in range(nevt):
+        col = IMPL.LCCollectionVec(EVENT.LCIO.MCPARTICLE) 
+        evt = IMPL.LCEventImpl() 
+
+        evt.setEventNumber(j)
+        evt.addCollection(col, "MCParticle")
+
+        phi =  random.random()*math.pi*2. #uniform distribution in phi
+        theta = math.acos(2*random.random()-1) #uniform distribution in theta
+                
+        px = pt*math.cos(phi)
+        py = pt*math.sin(phi)
+        pz = pt/math.tan(theta) 
+
+        # Momentum vector
+        momentum  = array('f', [ px, py, pz ])  
+
+        epx = decayLen*math.cos(phi)*math.sin(theta) 
+        epy = decayLen*math.sin(phi)*math.sin(theta)
+        epz = decayLen*math.cos(theta) 
+
+        # Decay position vector
+        endpoint = array('d',[ epx, epy, epz ] )  
+        
+
+#--------------- create MCParticle -------------------
+        mcp = IMPL.MCParticleImpl() 
+
+        mcp.setGeneratorStatus(genstat) 
+        mcp.setMass(mass)
+        mcp.setPDG(pdg) 
+        mcp.setMomentum(momentum)
+        mcp.setCharge(charge) 
+
+        if(decayLen < 1.e9):   
+            mcp.setEndpoint(endpoint) 
+#-------------------------------------------------------
+
+        col.addElement( mcp )
+        
+        wrt.writeEvent( evt )
+
+
+wrt.close() 
