9058c593a113cd0bf35a5740033d4b65d60a7d5c
Gregory Penn <gregorypenn@ap23.uc.osg-htc.org>
2025-03-12 16:28:26 -0500

    Adding track truth matching, and many comments to reco_steer.py

--- reconstruction/k4run/reco_steer.py
+++ reconstruction/k4run/reco_steer.py
@@ -8,7 +8,7 @@ parser.add_argument(
     "--DD4hepXMLFile",
     help="Compact detector description file",
     type=str,
-    default=os.environ.get("MUCOLL_GEO", ""),
+    default=os.environ.get("MAIA_GEO", ""),
 )
 
 parser.add_argument(
@@ -68,18 +68,20 @@ EventNumber.Parameters = {
                           "HowOften": ["1"]
                           }
 
+#https://github.com/iLCSoft/Marlin/blob/6f8703a389987082363206e41e5ff1054ed1f444/source/src/LCIOOutputProcessor.cc
 LCIOWriter_all = MarlinProcessorWrapper("LCIOWriter_all")
-LCIOWriter_all.OutputLevel = INFO
+LCIOWriter_all.OutputLevel = VERBOSE
 LCIOWriter_all.ProcessorType = "LCIOOutputProcessor"
 LCIOWriter_all.Parameters = {
                              "DropCollectionNames": [],
                              "DropCollectionTypes": [],
                              "FullSubsetCollections": [],
-                             "KeepCollectionNames": [],
+                             "KeepCollectionNames": ["SiTracks_Refitted", "SiTracks_Refitted_Relations", "MCParticle_SiTracks_Refitted"],
                              "LCIOOutputFile": ["output_reco.slcio"],
                              "LCIOWriteMode": ["WRITE_NEW"]
                              }
 
+#https://github.com/iLCSoft/Marlin/blob/6f8703a389987082363206e41e5ff1054ed1f444/source/src/LCIOOutputProcessor.cc
 LCIOWriter_light = MarlinProcessorWrapper("LCIOWriter_light")
 LCIOWriter_light.OutputLevel = INFO
 LCIOWriter_light.ProcessorType = "LCIOOutputProcessor"
@@ -87,11 +89,12 @@ LCIOWriter_light.Parameters = {
                                "DropCollectionNames": [],
                                "DropCollectionTypes": ["SimCalorimeterHit", "CalorimeterHit", "SimTrackerHit", "TrackerHitPlane", "Track", "LCRelation"],
                                "FullSubsetCollections": [],
-                               "KeepCollectionNames": ["SiTracks_Refitted", "SiTracks_Refitted_Relations", "PandoraPFOs", "MCPhysicsParticle"],
+                               "KeepCollectionNames": ["SiTracks_Refitted", "SiTracks_Refitted_Relations", "PandoraPFOs", "MCPhysicsParticle", "MCParticle_SiTracks_Refitted"],
                                "LCIOOutputFile": ["output_reco_light.slcio"],
                                "LCIOWriteMode": ["WRITE_NEW"]
                                }
 
+#https://github.com/MuonColliderSoft/ACTSTracking/blob/ce74f55a0ec320284ce8cc2d2d233a7f9c8b912d/src/ACTSSeededCKFTrackingProc.cxx#L45
 CKFTracking = MarlinProcessorWrapper("CKFTracking")
 CKFTracking.OutputLevel = INFO
 CKFTracking.ProcessorType = "ACTSSeededCKFTrackingProc"
@@ -101,33 +104,57 @@ CKFTracking.Parameters = {
     "MatFile": [the_args.MatFile],
     "PropagateBackward": ["False"],
     "RunCKF": ["True"],
-    "SeedFinding_CollisionRegion": ["3.5"],
-    "SeedFinding_DeltaRMax": ["60"],
-    "SeedFinding_DeltaRMin": ["2"],
-    "SeedFinding_DeltaRMaxBottom": ["50"],
-    "SeedFinding_DeltaRMaxTop": ["50"],
-    "SeedFinding_DeltaRMinBottom": ["5"],
-    "SeedFinding_DeltaRMinTop": ["2"],
+    "SeedFinding_CollisionRegion": ["3.5"], #this is 5 on Fede's (https://github.com/madbaron/SteeringMacros/blob/master/k4Reco/steer_reco_BIB_CONDOR.py#L44)
+    "SeedFinding_DeltaRMax": ["60"], #this is 80 on Fede's
+    "SeedFinding_DeltaRMin": ["2"], #this is 5 on Fede's
+    "SeedFinding_DeltaRMaxBottom": ["50"], #this doesn't exist on Fede's - it is 0 by default
+    "SeedFinding_DeltaRMaxTop": ["50"], #this doesn't exist on Fede's - it is 0 by default
+    "SeedFinding_DeltaRMinBottom": ["5"], #this doesn't exist on Fede's - it is 0 by default
+    "SeedFinding_DeltaRMinTop": ["2"], #this doesn't exist on Fede's - it is 0 by default
     "SeedFinding_ImpactMax": ["3"],
     "SeedFinding_MinPt": ["500"],
     "SeedFinding_RMax": ["150"],
-    "SeedFinding_ZMax": ["500"],
-    "SeedFinding_RadLengthPerSeed": ["0.1"],
-    "SeedFinding_zBottomBinLen": ["1"],
-    "SeedFinding_zTopBinLen": ["1"],
-    "SeedFinding_phiBottomBinLen": ["1"],
-    "SeedFinding_phiTopBinLen": ["1"],
-    "SeedFinding_SigmaScattering": ["3"],
+    "SeedFinding_ZMax": ["500"], #this doesn't exist on Fede's - it is 600 by default.
+    "SeedFinding_RadLengthPerSeed": ["0.1"], #this doesn't exist on Fede's. 0.1 is the default value, so this is consistent.
+    "SeedFinding_zBottomBinLen": ["1"], #this doesn't exist on Fede's. 1 is the default value, so this is consistent.
+    "SeedFinding_zTopBinLen": ["1"], #this doesn't exist on Fede's. 1 is the default value, so this is consistent.
+    "SeedFinding_phiBottomBinLen": ["1"], #this doesn't exist on Fede's. 1 is the default value, so this is consistent.
+    "SeedFinding_phiTopBinLen": ["1"], #this doesn't exist on Fede's. 1 is the default value, so this is consistent.
+    "SeedFinding_SigmaScattering": ["3"], #this is 50 on Fede's
     "SeedingLayers": [
         "13", "2", "13", "6", "13", "10", "13", "14", 
         "14", "2", "14", "6", "14", "10", "14", "14", 
         "15", "2", "15", "6", "15", "10", "15", "14",
-        ],
+        ], #this is different on Fede's
     "TGeoFile": [the_args.TGeoFile],
     "TrackCollectionName": ["AllTracks"],
-    "TrackerHitCollectionNames": ["VXDBarrelHits", "ITBarrelHits", "OTBarrelHits", "VXDEndcapHits", "ITEndcapHits", "OTEndcapHits"],
-    "CaloFace_Radius": ["1500"],
-    "CaloFace_Z": ["2307"]
+    "TrackerHitCollectionNames": ["VXDBarrelHits", "ITBarrelHits", "OTBarrelHits", "VXDEndcapHits", "ITEndcapHits", "OTEndcapHits"], # Fede has different names - may want to consider adding digi and reco together to more clearly harmonize naming choices
+    "CaloFace_Radius": ["1857"], #should be 1857, which is defeault anyways.
+    "CaloFace_Z": ["2307"] #default 
+    # 
+    # below is Fede's config for tracking 
+    # "CKF_Chi2CutOff": ["10"],
+    # "CKF_NumMeasurementsCutOff": ["1"],
+    # "MatFile": [the_args.MatFile],
+    # "PropagateBackward": ["False"],
+    # "RunCKF": ["True"],
+    # "SeedFinding_CollisionRegion": ["5"],
+    # "SeedFinding_DeltaRMax": ["80"],
+    # "SeedFinding_DeltaRMin": ["5"],
+    # "SeedFinding_ImpactMax": ["3"],
+    # "SeedFinding_MinPt": ["500"],
+    # "SeedFinding_RMax": ["150"],
+    # "SeedFinding_RadLengthPerSeed": ["0.1"],
+    # "SeedFinding_SigmaScattering": ["50"],
+    # "SeedingLayers": ["13", "2", "13", "6", "13", "10", "13", "14", 
+    #                   "14", "2", "14", "6", "14", "10", "14", "14", 
+    #                   "15", "2", "15", "6", "15", "10", "15", "14",
+    #                   "8", "2",
+    #                   "17", "2",
+    #                   "18", "2"],
+    # "TGeoFile": [the_args.TGeoFile],
+    # "TrackCollectionName": ["AllTracks"],
+    # "TrackerHitCollectionNames": ["VBTrackerHits", "IBTrackerHits", "OBTrackerHits", "VETrackerHits", "IETrackerHits", "OETrackerHits"]
 }
 
 TrackDeduplication = MarlinProcessorWrapper("TrackDeduplication")
@@ -146,7 +173,7 @@ TrackRefit.Parameters = {
                                 "EnergyLossOn": ["true"],
                                 "DoCutsOnRedChi2Nhits": ["true"],
                                 "ReducedChi2Cut": ["3."],
-                                "NHitsCuts": ["1,2 1 3,4 1 5,6 0 "], 
+                                #"NHitsCuts": ["1,2", "1", "3,4", "1", "5,6", "0"],
                                 "InputRelationCollectionName": ["SiTracksRelations"],
                                 "InputTrackCollectionName": ["SiTracks"],
                                 "Max_Chi2_Incr": ["1.79769e+30"],
@@ -160,16 +187,39 @@ TrackRefit.Parameters = {
                                 "MinClustersOnTrackAfterFit:": ["3"]
                                 }
 
+#adding track truth matching
+# https://github.com/MuonColliderSoft/ACTSTracking/blob/ce74f55a0ec320284ce8cc2d2d233a7f9c8b912d/src/TrackTruthProc.cxx#L20
+MyTrackTruth = MarlinProcessorWrapper("MyTrackTruth")
+MyTrackTruth.OutputLevel = INFO
+MyTrackTruth.ProcessorType = "TrackTruthProc"
+MyTrackTruth.Parameters = {
+    "MCParticleCollection": ["MCParticle"],
+    "Particle2TrackRelationName": ["MCParticle_SiTracks_Refitted"],
+    "TrackCollection": ["SiTracks_Refitted"],
+    "TrackerHit2SimTrackerHitRelationName": ["VXDBarrelHitsRelations", "ITBarrelHitsRelations", "OTBarrelHitsRelations", "VXDEndcapHitsRelations", "ITEndcapHitsRelations", "OTEndcapHitsRelations"]
+}
+
+# https://github.com/MuonColliderSoft/ACTSTracking/blob/ce74f55a0ec320284ce8cc2d2d233a7f9c8b912d/src/TrackTruthProc.cxx#L20
+MyTrackTruthSiTracks = MarlinProcessorWrapper("MyTrackTruthSiTracks")
+MyTrackTruthSiTracks.OutputLevel = INFO
+MyTrackTruthSiTracks.ProcessorType = "TrackTruthProc"
+MyTrackTruthSiTracks.Parameters = {
+    "MCParticleCollection": ["MCParticle"],
+    "Particle2TrackRelationName": ["MCParticle_SiTracks"],
+    "TrackCollection": ["SiTracks"],
+    "TrackerHit2SimTrackerHitRelationName": ["VXDBarrelHitsRelations", "ITBarrelHitsRelations", "OTBarrelHitsRelations", "VXDEndcapHitsRelations", "ITEndcapHitsRelations", "OTEndcapHitsRelations"]
+}
 
 DDMarlinPandora = MarlinProcessorWrapper("DDMarlinPandora")
 DDMarlinPandora.OutputLevel = INFO
 DDMarlinPandora.ProcessorType = "DDPandoraPFANewProcessor"
 DDMarlinPandora.Parameters = {
+                              "Verbosity": ["DEBUG0-4"],
                               "ClusterCollectionName": ["PandoraClusters"],
                               "CreateGaps": ["false"],
-                              "CurvatureToMomentumFactor": ["0.00015"],
-                              "D0TrackCut": ["200"],
-                              "D0UnmatchedVertexTrackCut": ["5"],
+                              "CurvatureToMomentumFactor": ["0.00015"], #is this correct? Looks like it is off by a factor of 2... it was 0.00015, I think 0.0003 is correct.
+                              "D0TrackCut": ["200"], # cut for track CanFormPFO
+                              "D0UnmatchedVertexTrackCut": ["5"], # used for CanFormClusterlessPFO
                               "DigitalMuonHits": ["0"],
                               "ECalBarrelNormalVector": ["0", "0", "1"],
                               "ECalCaloHitCollections": ["EcalBarrelCollectionRec", "EcalEndcapCollectionRec", "EcalPlugCollectionRec"], #this is for k4run digi
@@ -205,11 +255,11 @@ DDMarlinPandora.Parameters = {
                               "KinkVertexCollections": ["KinkVertices"],
                               "LayersFromEdgeMaxRearDistance": ["250"],
                               "MCParticleCollections": ["MCParticle"],
-                              "MaxBarrelTrackerInnerRDistance": ["200"],
+                              "MaxBarrelTrackerInnerRDistance": ["200"], # for track CanFormPFO requirements
                               "MaxClusterEnergyToApplySoftComp": ["2000."],
                               "MaxHCalHitHadronicEnergy": ["1000000"],
                               "MaxTrackHits": ["5000"],
-                              "MaxTrackSigmaPOverP": ["0.15"],
+                              "MaxTrackSigmaPOverP": ["0.15"], # track quality cut. Upper bound.
                               "MinBarrelTrackerHitFractionOfExpected": ["0"],
                               "MinCleanCorrectedHitEnergy": ["0.1"],
                               "MinCleanHitEnergy": ["0.5"],
@@ -218,7 +268,7 @@ DDMarlinPandora.Parameters = {
                               "MinFtdTrackHits": ["0"],
                               "MinMomentumForTrackHitChecks": ["0"],
                               "MinTpcHitFractionOfExpected": ["0"],
-                              "MinTrackECalDistanceFromIp": ["0"],
+                              "MinTrackECalDistanceFromIp": ["0"], # track quality cut. Lower bound.
                               "MinTrackHits": ["0"],
                               "MuonBarrelBField": ["-1.34"],
                               "MuonCaloHitCollections": ["MuonHits"],
@@ -237,9 +287,9 @@ DDMarlinPandora.Parameters = {
                               "ReachesECalMinFtdLayer": ["0"],
                               "ReachesECalNBarrelTrackerHits": ["0"],
                               "ReachesECalNFtdHits": ["0"],
-                              "RelCaloHitCollections": ["CaloHitsRelations", "MuonHitsRelations"],
+                              # "RelCaloHitCollections": ["CaloHitsRelations", "MuonHitsRelations"],
                               "RelTrackCollections": ["SiTracks_Refitted_Relations"],# for track refitting
-                              # "RelTrackCollections": ["SiTracks_Relations"],
+                              #"RelTrackCollections": ["SiTracks_Relations"],
                               "ShouldFormTrackRelationships": ["1"],
                               "SoftwareCompensationEnergyDensityBins": ["0", "2.", "5.", "7.5", "9.5", "13.", "16.", "20.", "23.5", "28.", "33.", "40.", "50.", "75.", "100."],
                               "SoftwareCompensationWeights": ["1.61741", "-0.00444385", "2.29683e-05", "-0.0731236", "-0.00157099", "-7.09546e-07", "0.868443", "1.0561", "-0.0238574"],
@@ -248,21 +298,21 @@ DDMarlinPandora.Parameters = {
                               "StartVertexCollectionName": ["PandoraStartVertices"],
                               "StripSplittingOn": ["0"],
                               "TrackCollections": ["SiTracks_Refitted"], #for track refitting
-                              # "TrackCollections": ["SiTracks"],
+                              #"TrackCollections": ["SiTracks"],
                               "TrackCreatorName": ["DDTrackCreatorCLIC"],
                               "TrackStateTolerance": ["0"],
                               "TrackSystemName": ["DDKalTest"],
-                              "UnmatchedVertexTrackMaxEnergy": ["5"],
+                              "UnmatchedVertexTrackMaxEnergy": ["5"], #for track CanFormClusterlessPFO requirements
                               "UseEcalScLayers": ["0"],
-                              "UseNonVertexTracks": ["1"],
+                              "UseNonVertexTracks": ["1"], # for track CanFormPFO requirements. Setting to 1 is the loose, 0 is tight. 
                               "UseOldTrackStateCalculation": ["0"],
-                              "UseUnmatchedNonVertexTracks": ["0"],
+                              "UseUnmatchedNonVertexTracks": ["0"], # for track CanFormClusterlessPFO requirements. Setting to 1 is loose, 0 is tight.
                               "UseUnmatchedVertexTracks": ["1"],
                               "V0VertexCollections": ["V0Vertices"],
                               "YokeBarrelNormalVector": ["0", "0", "1"],
-                              "Z0TrackCut": ["200"],
-                              "Z0UnmatchedVertexTrackCut": ["5"],
-                              "ZCutForNonVertexTracks": ["250"]
+                              "Z0TrackCut": ["200"], # for track CanFormPFO requirements
+                              "Z0UnmatchedVertexTrackCut": ["5"], #for track CanFormClusterlessPFO requirements
+                              "ZCutForNonVertexTracks": ["250"] # for track CanFormPFO requirements
                               }
 
 PFOSelection = MarlinProcessorWrapper("PFOSelection")
@@ -315,6 +365,36 @@ PFOSelection.Parameters = {
                            "UseClusterLessPfos": ["1"],
                            "UseNeutronTiming": ["0"]
                            }
+RecoMCTruthLinker = MarlinProcessorWrapper("MyRecoMCTruthLinker")
+RecoMCTruthLinker.OutputLevel = INFO
+RecoMCTruthLinker.ProcessorType = "RecoMCTruthLinker"
+RecoMCTruthLinker.Parameters = {
+                            "MyRecoMCTruthLinker": ["1"],
+                            "CalohitMCTruthLinkName": ["CalohitMCTruthLink"],
+                            "ClusterCollection": ["MergedClusters"],
+                            "ClusterMCTruthLinkName": ["ClusterMCTruthLink"],
+                            "FullRecoRelation": ["false"],
+                            "InvertedNonDestructiveInteractionLogic": ["false"],
+                            "KeepDaughtersPDG": ["22", "111", "310", "13", "211", "321", "3120"],
+                            "MCParticleCollection": ["MCParticle"],
+                            "MCParticlesSkimmedName": ["MCParticlesSkimmed"],
+                            "MCTruthClusterLinkName": [],
+                            "MCTruthRecoLinkName": [],
+                            "MCTruthTrackLinkName": [],
+                            "RecoMCTruthLinkName": ["RecoMCTruthLink"],
+                            "RecoParticleCollection": ["MergedRecoParticles"],
+                            "SaveBremsstrahlungPhotons": ["false"],
+                            "SimCaloHitCollections": ["ECalBarrelCollection", "ECalEndcapCollection", "ECalPlugCollection", "HCalBarrelCollection", "HCalEndcapCollection", "HCalRingCollection", "YokeBarrelCollection", "YokeEndcapCollection"],
+                            "SimCalorimeterHitRelationNames": ["RelationCaloHit", "RelationMuonHit"],
+                            "SimTrackerHitCollections": ["VertexBarrelCollection", "VertexEndcapCollection", "InnerTrackerBarrelCollection", "OuterTrackerBarrelCollection", "InnerTrackerEndcapCollection", "OuterTrackerEndcapCollection"],
+                            "TrackCollection": ["SiTracks_Refitted"],
+                            "TrackMCTruthLinkName": ["SiTracksMCTruthLink"],
+                            "TrackerHitsRelInputCollections": ["VXDTrackerHitRelations", "VXDEndcapTrackerHitRelations", "InnerTrackerBarrelHitsRelations", "OuterTrackerBarrelHitsRelations", "InnerTrackerEndcapHitsRelations", "OuterTrackerEndcapHitsRelations"],
+                            "UseTrackerHitRelations": ["true"],
+                            "UsingParticleGun": ["false"],
+                            "Verbosity": ["MESSAGE"], #("DEBUG0-4,MESSAGE0-4,WARNING0-4,ERROR0-4,SILENT")
+                            "daughtersECutMeV": ["10"]
+                            }
 
 FastJetProcessor = MarlinProcessorWrapper("FastJetProcessor")
 FastJetProcessor.OutputLevel = INFO
@@ -334,16 +414,19 @@ algList.append(DD4hep)
 algList.append(CKFTracking)
 algList.append(TrackDeduplication)
 algList.append(TrackRefit)
+algList.append(MyTrackTruth)
+algList.append(MyTrackTruthSiTracks)
 algList.append(DDMarlinPandora)
 algList.append(PFOSelection)
+#algList.append(RecoMCTruthLinker)
 algList.append(FastJetProcessor)
 algList.append(LCIOWriter_all)
-algList.append(LCIOWriter_light)
+#algList.append(LCIOWriter_light)
 
 from Configurables import ApplicationMgr
 ApplicationMgr( TopAlg = algList,
                 EvtSel = 'NONE',
-                EvtMax   = -1,
+                EvtMax   = -1, #-1
                 ExtSvc = [evtsvc],
                 OutputLevel=INFO
               )
